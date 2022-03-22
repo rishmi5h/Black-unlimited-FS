723e46c4ab35453431fe1e652052de16d5d774aa
RISHABH MISHRA <rish.mish.2607@gmail.com>
2022-03-22 11:45:48 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import {
+  Authentication,
+  Cart,
+  Home,
+  Product,
+  Products,
+  Profile,
+  Wishlist,
+} from "./pages";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='cart' element={<Cart />} />
+        <Route path='product' element={<Product />} />
+        <Route path='products' element={<Products />} />
+        <Route path='profile' element={<Profile />} />
+        <Route path='wishlist' element={<Wishlist />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { Authentication } from "./Authentication";
+export { Cart } from "./Cart";
+export { Home } from "./Home";
+export { Product } from "./Product";
+export { Products } from "./Products";
+export { Profile } from "./Profile";
+export { Wishlist } from "./Wishlist";
