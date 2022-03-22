5e72d1010d43c1538db160b874f4992ce8738450
RISHABH MISHRA <rish.mish.2607@gmail.com>
2022-03-23 00:31:09 +0530

    added / for urls  🥲

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ const App = () => {
     <>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='cart' element={<Cart />} />
-        <Route path='product' element={<Product />} />
-        <Route path='products' element={<Products />} />
-        <Route path='profile' element={<Profile />} />
-        <Route path='wishlist' element={<Wishlist />} />
+        <Route path='/auth' element={<Authentication />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/wishlist' element={<Wishlist />} />
       </Routes>
     </>
   );
